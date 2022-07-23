c75519307ac98f78948c5058944367b5f0951584
AlyHamdy10 <aly.hamdy.ibrahim@gmail.com>
2022-07-23 22:06:13 +0200

    adding dfs_stack
    depth first traversal using stack

--- graphs.py
+++ graphs.py
@@ -1,1 +1,8 @@
 class Graphs:
+    def dfs_stack(self, graph, source):  # depth first traversal using stack
+        stack = [source]
+        while len(stack) != 0:
+            current = stack.pop()
+            print(current)
+            for neighbour in graph[current]:
+                stack.append(neighbour)
